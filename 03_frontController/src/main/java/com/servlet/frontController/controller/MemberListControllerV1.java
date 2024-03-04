704865cf3644847b2b1186ec06f857f245b14739
SMJin <sstaw@naver.com>
2024-03-04 22:46:52 +0900

    frontController 도입

---
+++ 03_frontController/src/main/java/com/servlet/frontController/controller/MemberListControllerV1.java
@@ -0,0 +1,27 @@
+package com.servlet.frontController.controller;
+
+import com.servlet.frontController.ControllerV1;
+import com.servlet.frontController.domain.Member;
+import com.servlet.frontController.domain.MemberRepository;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV1 implements ControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+
+        request.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ 03_frontController/src/main/webapp/WEB-INF/views/save.jsp
@@ -0,0 +1,25 @@
+<%@ page import="com.hello.mvc.domain.MemberRepository" %>
+<%@ page import="com.hello.mvc.domain.Member" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+     MemberRepository memberRepository = MemberRepository.getInstance();
+     System.out.println("save.jsp");
+     String username = request.getParameter("username");
+     int age = Integer.parseInt(request.getParameter("age"));
+     Member member = new Member(username, age);
+     System.out.println("user = " + member);
+     MemberRepository.save(member);
+%>
+<html>
+<head>
+ <meta charset="UTF-8">
+</head>
+<body>
+성공<ul>
+ <li>id=<%=member.getId()%></li>
+ <li>username=<%=member.getUsername()%></li>
+ <li>age=<%=member.getAge()%></li>
+</ul>
+<a href="/index.html">메인</a>
+</body>
+</html>
