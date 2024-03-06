8156b59c012a09bd2724cf3350844012cf2e7f60
SMJin <sstaw@naver.com>
2024-03-07 00:13:49 +0900

    mvcStructure::HandlerMapping/Adapter

---
+++ 04_mvcStructure/src/main/java/com/spring/mvcStructure/MvcStructureApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mvcStructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcStructureApplication.class, args);
+	}
+
+}
