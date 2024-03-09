88864dcb44b0d6c830601a1fc3ce16b5df29387e
SMJin <sstaw@naver.com>
2024-03-09 16:23:57 +0900

    mvcBasic::Slf4j

---
+++ 05_mvcBasic/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+
+        // 각 로그별로 레벨이 다르다
+        // (낮은 레벨) trace -> debug -> info -> warn -> error (높은 레벨)
+        log.trace("trace log={}", name);    //
+        log.debug("debug log={}", name);    // debug - 개발서버같은 곳
+        log.info("  info log={}", name);    // 중요한 정보
+        log.warn("  warn log={}", name);    // 경고
+        log.error("error log={}", name);    // error
+
+        return "ok";
+    }
+}
