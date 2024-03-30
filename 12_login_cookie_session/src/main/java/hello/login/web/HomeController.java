492168f4597ee0f190558adfa8941dbbb993261a
SMJin <sstaw@naver.com>
2024-03-31 00:09:12 +0900

    session :: httpServletReqeust.getSession()

--- 12_login_cookie_session/src/main/java/hello/login/web/HomeController.java
+++ 12_login_cookie_session/src/main/java/hello/login/web/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -43,11 +44,30 @@ public class HomeController {
 //        return "loginHome";
 //    }
 
+//    @GetMapping("/")
+//    public String homeLoginV2(HttpServletRequest request, Model model) {
+//
+//        Member loginMember = (Member) sessionManager.getSession(request);
+//
+//        if (loginMember == null) {
+//            return "home";
+//        }
+//
+//        model.addAttribute("member", loginMember);
+//
+//        return "loginHome";
+//    }
+
     @GetMapping("/")
-    public String homeLogin(HttpServletRequest request, Model model) {
+    public String homeLoginV3(HttpServletRequest request, Model model) {
 
-        Member loginMember = (Member) sessionManager.getSession(request);
+        // 로그인 사용자가 없을 때도 세션이 생성되면 안되기 때문에 create 파라미터는 false로 지정
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
 
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
         if (loginMember == null) {
             return "home";
         }
