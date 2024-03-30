89e5513b2b879aad5a9b275a8b4425314d35917d
SMJin <sstaw@naver.com>
2024-03-30 23:39:51 +0900

    Session 직점ì접 만들기 :: SessionManager

---
+++ 12_login_cookie_session/src/main/java/hello/login/web/session/SessionManager.java
@@ -0,0 +1,80 @@
+package hello.login.web.session;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+/*
+세션 관리
+ */
+@Component
+public class SessionManager {
+
+    public static final String SESSION_COOKIE_NAME = "mySessionId";
+    private Map<String, Object> sessionStore = new ConcurrentHashMap<>();
+
+    /*
+    1. 세션 생성
+        sessionId 생성 (임의의 추정 불가능한 랜덤 값)
+        세션 저장소에 sessionId와 보관할 값 저장
+        sessionId로 응답 쿠키를 생성해서 클라이언트에 전달
+     */
+    public void createSession(Object value, HttpServletResponse response) {
+
+        // sessionId 생성 (임의의 추정 불가능한 랜덤 값)
+        String sessionId = UUID.randomUUID().toString();
+
+        // 세션 저장소에 sessionId와 보관할 값 저장
+        sessionStore.put(sessionId, value);
+
+        // sessionId로 응답 쿠키를 생성해서 클라이언트에 전달
+        Cookie mySessionCookie = new Cookie(SESSION_COOKIE_NAME, sessionId);
+        response.addCookie(mySessionCookie);
+    }
+
+    /*
+    2. 세션 조회
+        클라이언트가 요청한 sessionId 쿠키의 값으로, 세션 저장소에 보관한 값 조회
+     */
+    public Object getSession(HttpServletRequest request) {
+        Cookie sessionCookie = findCookies(request, SESSION_COOKIE_NAME);
+        if (sessionCookie == null) {
+            return null;
+        }
+        return sessionStore.get(sessionCookie.getValue());
+    }
+
+    public Cookie findCookies(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+
+//        for (Cookie cookie : cookies) {
+//            if (cookie.getName().equals(SESSION_COOKIE_NAME)) {
+//                return sessionStore.get(cookie.getValue());
+//            }
+//        }
+
+        return Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals(cookieName))
+                .findFirst()
+                .orElse(null);
+    }
+
+    /*
+    3. 세션 만료
+        클라이언트가 요청한 sessionId 쿠키의 값으로, 세션 저장소에 보관한 sessionId와 값 제거
+     */
+    public void expire(HttpServletRequest request) {
+        Cookie sessionCookie = findCookies(request, SESSION_COOKIE_NAME);
+        if (sessionCookie != null) {
+            sessionStore.remove(sessionCookie.getValue());
+        }
+    }
+}
