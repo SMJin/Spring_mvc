c4a985be6461fe5f0adb520db623c9b0affa4fa6
SMJin <sstaw@naver.com>
2024-04-03 00:36:03 +0900

    Spring Exception :: DefaultHandlerExceptionResolver

--- 15_api_exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ 15_api_exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -8,11 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
-@RestController
+@RestController(value = "/api/")
 public class ApiExceptionController {
 
     @GetMapping("/api/members/{id}")
@@ -40,8 +41,12 @@ public class ApiExceptionController {
 
     @GetMapping("/api/response-status-ex2")
     public String responseStatusEx2() {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new
-                IllegalArgumentException());
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
     }
 
     @Data
